0f6a5cb91fa5e678efc9840c4fa52da24d25328b
Vidhi <vidhi2005ajmera@gmail.com>
2024-11-12 16:37:10 +0530

    change

--- seizure-detection-frontend/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div>
-      <h2>Welcome to the Seizure Detection App</h2>
-    </div>
-  );
-};
-
-export default Home;
--- seizure-detection-frontend/src/sockets/socket.js
+++
@@ -1,4 +0,0 @@
-import io from "socket.io-client";
-
-const SOCKET_URL = "http://localhost:5000"; // Your backend address
-export const socket = io(SOCKET_URL);
